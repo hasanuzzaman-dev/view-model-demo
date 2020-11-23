aaa0296ec25c33e66236f225a0462ac0730fd06c
Hasan <hasanuzzaman.dev@gmail.com>
2020-11-23 14:44:39 +0600

    add LiveData

--- app/src/main/java/com/hasan/viewmodeldemo/MainActivity.java
+++ app/src/main/java/com/hasan/viewmodeldemo/MainActivity.java
@@ -1,17 +1,22 @@
 package com.hasan.viewmodeldemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private TextView numberTV;
+    private Button button;
     private MainActivityViewModel mainActivityViewModel;
 
     @Override
@@ -20,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         numberTV = findViewById(R.id.numberTV);
+        button = findViewById(R.id.button);
 
         /*** Without viewModel ***/
         /*MainActivityDataGenerator myData = new MainActivityDataGenerator();
@@ -29,10 +35,33 @@ public class MainActivity extends AppCompatActivity {
 
         //deprecated
         //MainActivityViewModel mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
-        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-
+       /*
+       //viewModel
+       mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
         String myRandomNumber = mainActivityViewModel.getNumber();
         numberTV.setText(myRandomNumber);
+
+        */
+
+        /**** viewModel and Live Data***/
+
+        MainActivityViewModel mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        LiveData<String> myRandomNumber = mainActivityViewModel.getMyRandomNumber();
+
+        myRandomNumber.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                numberTV.setText(s);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivityViewModel.createNumber();
+            }
+        });
+
 
         Log.d(TAG, "onCreate: Random Number Set");
         
--- app/src/main/java/com/hasan/viewmodeldemo/MainActivityViewModel.java
+++ app/src/main/java/com/hasan/viewmodeldemo/MainActivityViewModel.java
@@ -2,6 +2,8 @@ package com.hasan.viewmodeldemo;
 
 import android.util.Log;
 
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.Random;
@@ -10,12 +12,12 @@ public class MainActivityViewModel extends ViewModel {
 
     private String TAG = this.getClass().getSimpleName();
 
+/*
     private String myRandomNumber;
-
-    public String getNumber(){
+    public String getNumber() {
         Log.d(TAG, "getNumber: started");
 
-        if (myRandomNumber == null){
+        if (myRandomNumber == null) {
             createNumber();
         }
 
@@ -25,7 +27,27 @@ public class MainActivityViewModel extends ViewModel {
     private void createNumber() {
         Log.d(TAG, "createNumber: started");
         Random random = new Random();
-        myRandomNumber = "Number: "+(random.nextInt(10 - 1) - 1);
+        myRandomNumber = "Number: " + (random.nextInt(10 - 1) - 1);
+    }*/
+
+    //Live Data
+    private MutableLiveData<String> myRandomNumber;
+
+    public MutableLiveData<String> getMyRandomNumber(){
+        Log.d(TAG, "getMyRandomNumber: started");
+        if (myRandomNumber == null){
+            myRandomNumber = new MediatorLiveData<>();
+            createNumber();
+        }
+
+        return myRandomNumber;
+    }
+
+    public void createNumber() {
+        Log.d(TAG, "createNumber: started!");
+        Random random = new Random();
+        myRandomNumber.setValue("Number: "+(random.nextInt(10 - 1) + 1));
+
     }
 
     @Override
